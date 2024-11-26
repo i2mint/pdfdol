a7aeb20bfcf23ef20c3ca72f1dfd47d179b18aa3
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-11-26 13:00:26 +0100

    feat: enhance concat_pdfs to support image concatenation and improve filtering options

--- pdfdol/util.py
+++ pdfdol/util.py
@@ -7,9 +7,12 @@ import os
 from pathlib import Path
 from contextlib import redirect_stderr, nullcontext, suppress
 
-from dol import Pipe, filt_iter, cache_iter
+from dol import Pipe, filt_iter, wrap_kvs
 
 filter_pdfs = filt_iter.suffixes('.pdf')
+filter_pdfs_and_images = filt_iter.suffixes(
+    ('.pdf', '.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')
+)
 
 PdfPages = Iterable[PageObject]
 Filepath = str
@@ -192,6 +195,30 @@ else:
 
 bytes_to_pdf_reader_obj = Pipe(io.BytesIO, PdfReader)
 
+
+def get_file_extension(filename):
+    return os.path.splitext(filename)[1].lower()
+
+
+def image_bytes_to_pdf_bytes(image_bytes):
+    """Convert image bytes to PDF bytes."""
+    from PIL import Image  # pip install Pillow
+
+    image = Image.open(io.BytesIO(image_bytes))
+    if image.mode in ('RGBA', 'LA'):
+        image = image.convert('RGB')
+    output = io.BytesIO()
+    image.save(output, format='PDF')
+    return output.getvalue()
+
+
+def key_and_bytes_to_pdf_bytes(key, data_bytes):
+    extension = get_file_extension(key)
+    if extension in {'.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff'}:
+        return image_bytes_to_pdf_bytes(data_bytes)
+    return data_bytes
+
+
 from operator import methodcaller
 
 # equivalent to lambda pdf_filepath: Path(pdf_filepath).write_bytes():
@@ -203,13 +230,9 @@ file_to_bytes = Pipe(Path, methodcaller('read_bytes'))
 def concat_pdf_readers(pdf_readers: Iterable[PdfReader]) -> PdfWriter:
     """Concatenate multiple PdfReader objects into a single PdfWriter object."""
     writer = PdfWriter()
-
-    # Iterate over the iterable of PdfReader objects
     for reader in pdf_readers:
-        # Append all pages of the current pdf to the writer object
-        for page_num in range(len(reader.pages)):
-            writer.add_page(reader.pages[page_num])
-
+        for page in reader.pages:
+            writer.add_page(page)
     return writer
 
 
@@ -236,11 +259,12 @@ def concat_pdfs(
     pdf_source: Mapping[str, bytes],
     save_filepath=None,
     *,
-    filter_pdf_extension=False,
+    filter_extensions=False,
     key_order: Union[Callable, Iterable] = None,
+    **kwargs,
 ) -> Union[str, bytes]:
     """
-    Concatenate multiple PDFs given as a mapping of filepaths to bytes.
+    Concatenate multiple PDFs and/or images given as a mapping of filepaths to bytes.
 
     Tip: Pdfs are aggregated in the order of the mapping's iteration order.
     If you need these to be in a specific order, you can use the key_order argument
@@ -254,7 +278,8 @@ def concat_pdfs(
         If `None`, the save_filepath will be taken from the rootdir of the pdf_source
         that attribute exists, and no file of that name (+'.pdf') exists.
         If False, the pdf bytes are returned.
-    :param filter_pdf_extension: If True, only pdf files are considered
+    :param filter_extensions: If True, only files with recognized extensions
+        ('.pdf', '.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff') are considered
     :param key_order: Callable or iterable of keys to sort the mapping
 
     :return: The save_filepath if it was specified, otherwise the concatenated pdf bytes
@@ -263,12 +288,22 @@ def concat_pdfs(
     >>> concat_pdfs(s, key_order=sorted)  # doctest: +SKIP
 
     """
-    if filter_pdf_extension:
-        pdf_source = filter_pdfs(pdf_source)
-    if key_order is not None:
-        pdf_source = cache_iter(pdf_source, keys_cache=key_order)
+    filter_extensions = kwargs.get(
+        'filter_pdf_extension', filter_extensions
+    )  # backwards compatibility
 
-    pdf_bytes = pdf_source.values()
+    if filter_extensions:
+        pdf_source = filter_pdfs_and_images(pdf_source)
+
+    if key_order is not None:
+        if callable(key_order):
+            keys = sorted(pdf_source.keys(), key=key_order)
+        else:
+            keys = key_order
+        pdf_source = {k: pdf_source[k] for k in keys}
+
+    _pdf_source = wrap_kvs(pdf_source, postget=key_and_bytes_to_pdf_bytes)
+    pdf_bytes = _pdf_source.values()
     combined_pdf_bytes = concat_pdf_bytes(pdf_bytes)
 
     if save_filepath is False:
@@ -290,4 +325,3 @@ def concat_pdfs(
 
     Path(save_filepath).write_bytes(combined_pdf_bytes)
     return save_filepath
-
